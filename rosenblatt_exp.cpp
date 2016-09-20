9217386337f763ca83c2466a490eb3b7bd24d15d
shushm <huamingshen@yahoo.com>
2016-09-20 16:27:26 +0800

    Add files via upload

---
+++ rosenblatt_exp.cpp
@@ -0,0 +1,151 @@
+// rosenblatt_exp.cpp : 定义控制台应用程序的入口点。
+// 样本为两维度，线性可分
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+double eta = 0.1;//学习率
+double w[2] = { 0.0, 0.0 }, b = 0.0;//定义参数
+
+struct DataSet
+{
+	double x1;
+	double x2;
+	int y;
+};
+
+int i;
+int n = 0;//迭代次数
+const int num = 10;//数据集大小（估计）
+int Datacount = 0;//数据集大小（实际）
+bool flag = true;//是否误分类
+DataSet Sampledata[num];
+
+//从文件中读取数据
+int readData(DataSet data[])
+{
+	ifstream file;
+	file.open("data.txt");
+	int i = 0;
+	while (!file.eof())
+	{
+		file >> data[i].x1 >> data[i].x2 >> data[i].y;
+		i++;
+	}
+	file.close();
+	return i;
+}
+
+int main()
+{
+
+	Datacount=readData(Sampledata);//读入数据
+
+	//输出数据集
+	cout << "数据集为：" << endl;
+	for (i = 0; i < Datacount; i++)
+	{
+		cout << Sampledata[i].x1 << "  " << Sampledata[i].x2 << "  " << Sampledata[i].y << endl;
+	}
+
+	while (flag)
+	{
+		for (i = 0; i < Datacount; i++)
+		{
+			flag = false;
+			if (Sampledata[i].y * (w[0] * Sampledata[i].x1 + w[1] * Sampledata[i].x2 + b) <= 0)
+			{
+				flag = true;
+				w[0] = w[0] + eta * Sampledata[i].y * Sampledata[i].x1;
+				w[1] = w[1] + eta * Sampledata[i].y * Sampledata[i].x2;
+				b = b + eta * Sampledata[i].y;
+				n++;
+				break;
+			}
+		}
+	}
+
+	cout << endl << "结果：" << endl;
+	cout << "w = " << w[0] << ", " << w[1] << " " << "b=" << b << endl;
+	cout << "迭代次数：" << n << endl;
+
+	return 0;
+}
+
+/*
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+const int num = 10;//数据集大小（估计）
+int count = 0;//数据集大小（实际）
+bool flag = true;//是否误分类
+
+struct DataSet
+{
+double x1;
+double x2;
+int y;
+}data[num];
+
+double eta = 1.0;//学习率
+double w[2] = {0.0, 0.0}, b = 0.0;//定义参数
+
+//从文件中读取数据
+void readData()
+{
+ifstream file;
+file.open("data.dat");
+int i = 0;
+while(!file.eof())
+{
+file >> data[i].x1 >> data[i].x2 >> data[i].y;
+i++;
+count++;
+}
+file.close();
+}
+
+int main()
+{
+int i;
+int n = 0;//迭代次数
+readData();//读入数据
+
+//输出数据集
+cout << "数据集为：" << endl;
+for(i = 0; i < count; i++)
+{
+cout << data[i].x1 << "  " << data[i].x2 << "  " << data[i].y << endl;
+}
+
+while(flag)
+{
+for(i = 0; i < count; i++)
+{
+flag = false;
+if( data[i].y * (w[0] * data[i].x1 + w[1] * data[i].x2 + b) <= 0)
+{
+flag = true;
+w[0] = w[0] + eta * data[i].y * data[i].x1;
+w[1] = w[1] + eta * data[i].y * data[i].x2;
+b = b + eta * data[i].y;
+n++;
+break;
+}
+}
+}
+
+cout << endl << "结果：" << endl;
+cout << "w = " << w[0] << ", " << w[1] << " " << "b=" << b << endl;
+cout << "迭代次数：" << n << endl;
+
+return 0;
+}
+
+*/
